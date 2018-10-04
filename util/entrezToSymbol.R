3d2b1ee1a2cf26202e62f849f375ed6f049dc337
Nicholas Rachmaninoff <rachmaninoffn2@ai-rstudioprd1.niaid.nih.gov>
2018-10-04 12:24:08 -0400

    changed directory structure and made processing into .Rmd

--- util/entrezToSymbol.R
+++ util/entrezToSymbol.R
@@ -1,8 +1,13 @@
 library('biomaRt')
 
 entrezToSymbol <- function(ensembl){
+  ### Returns a dataframe with ensembl ids and HGNC symbols
+  
+  ### ensembl ###
+  # input: a vector of ensembl gene ID's
+  
   mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
-  genes <- ensembl.ids
+  genes <- ensembl
   G_list <- getBM(filters= "ensembl_gene_id", attributes= c("ensembl_gene_id",
                                                             "hgnc_symbol"),values=genes,mart= mart)
   return(G_list)
